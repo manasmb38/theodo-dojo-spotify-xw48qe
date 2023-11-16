f9f7081e7d95d2d2b6b63249c1cba6839c3250e9
manasmb38 <nathnathmb@gmail.com>
2023-11-16 18:29:05 +0100

    '"fecth Tracks"

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 const trackUrls = [
   'https://p.scdn.co/mp3-preview/742294f35af9390e799dd96c633788410a332e52',
@@ -10,7 +11,7 @@ const trackUrls = [
 ];
 
 
-import { useState } from 'react';
+
 
 const App = () => {
   const [trackIndex, setTrackIndex] = useState(0)
@@ -26,6 +27,7 @@ const goToNextTrack = () => {
       <div className="App-images">
         <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
       </div>
+     
       <audio src={trackUrls[trackIndex]} autoPlay controls />
       <button onClick={goToNextTrack}>
         Next track
