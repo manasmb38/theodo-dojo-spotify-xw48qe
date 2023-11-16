800a627f46e5d5f72971a22dff04e0bffd123d8b
manasmb38 <nathnathmb@gmail.com>
2023-11-16 18:34:42 +0100

    "API"

--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,4 +1,4 @@
-const apiToken = '';
+const apiToken = 'BQCmnRlftrNF62ddKCphdcRUxvPA_N07wEB4Y6wDUPI-9Ykr-ntFAKJlsJ0ICq1-R-afVYFoBvNz1c0m3ti8cZBISxUr21zcG7avxPq52nNmPB9fwiI9CZ1AJ0YIfdV-X4d9JHAdzxUEb_HHtNXBKqXv5sR4cjfqRpxI6ovh8a7v_LherPfYUlisu-Ue1IutCwyjIlooaISqdetGe8yk1tjUzw';
 
 export const fetchTracks = async () => {
   const response = await fetch('https://api.spotify.com/v1/me/tracks', {
